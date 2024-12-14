e1a60c58b6fc229638443300d4778da21b38e750
rdelcoig <romain@rdelcoig.be>
2024-12-14 21:43:14 +0100

    day 06 init

--- day_06/day06.c
+++ day_06/day06.c
@@ -8,23 +8,31 @@
 #include "day06.h"
 #include "../utils.h"
 
-void read_file_day05(const char *path, int ***map) {
+static void read_file_day06(const char *path, int ***map) {
     FILE *file = fopen(path, "r");
+
+    if (file == NULL) {
+        perror("File not found");
+        exit(1);
+    }
 
     const int size = 12;
     char line[size];
     while (fgets(line, size, file) != NULL) {
-        printf("%s", line);
+        // printf("%d %d %d : %s", line[9], line[10], line[11], line);
+        //printf("%d : %s", line[9], line);
     }
 
     fclose(file);
 }
 
-void set_day05_answer(Answer2Parts *answer) {
+void set_day06_answer(Answer2Parts *answer) {
     const char *path = "../day_06/day06_test.txt";
     //const char *path = "../day_06/day06.txt";
 
     int **map = NULL;
+
+    read_file_day06(path, &map);
 
     answer->part_1 = 41;
     answer->part_2 = 0;
