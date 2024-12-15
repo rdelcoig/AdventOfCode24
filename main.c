324a34dd0548ecbd5da272e5c4079ce58002678d
rdelcoig <romain@rdelcoig.be>
2024-12-15 19:17:54 +0100

    cleaning

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 #include "common/utils.h"
-#include "control.h"
 #include "day_01/day01.h"
 #include "day_02/day02.h"
 #include "day_03/day03.h"
@@ -9,25 +8,38 @@
 #include "day_05/day05.h"
 #include "day_06/day06.h"
 
-static void print_answer(void (*set_answer)(Answer2Parts *), void (*control_day)(const Answer2Parts *), const int day) {
-    Answer2Parts answer;
-
-    set_answer(&answer);
-
-    printf("\nDay %02d results\n--------------\n", day);
-    printf("Part 1 : %d\n", answer.part_1);
-    printf("Part 2 : %d\n", answer.part_2);
-
-    control_day(&answer);
-}
-
 int main(void) {
-    print_answer(set_day01_answer, control_day_01, 1);
-    print_answer(set_day02_answer, control_day_02, 2);
-    print_answer(set_day03_answer, control_day_03, 3);
-    print_answer(set_day04_answer, control_day_04, 4);
-    print_answer(set_day05_answer, control_day_05, 5);
-    print_answer(set_day06_answer, control_day_06, 6);
+    const int days = 6;
+
+    const Answer2Parts control_answers[] = {
+        {1879048, 21024792},
+        {631, 665},
+        {175615763, 74361272},
+        {2358, 1737},
+        {5268, 5799},
+        {5080, 0}
+    };
+
+    Answer2Parts answers[days];
+    set_day01_answer(&answers[0]);
+    set_day02_answer(&answers[1]);
+    set_day03_answer(&answers[2]);
+    set_day04_answer(&answers[3]);
+    set_day05_answer(&answers[4]);
+    set_day06_answer(&answers[5]);
+
+    for (int i = 0; i < days; i++) {
+        const int day = i + 1;
+        const Answer2Parts answer = answers[i];
+        printf("\nDay %02d results\n--------------\n", day);
+        printf("Part 1 : %d\n", answer.part_1);
+        printf("Part 2 : %d\n", answer.part_2);
+
+        const Answer2Parts control = control_answers[i];
+        if (answer.part_1 != control.part_1 || answer.part_2 != control.part_2) {
+            printf("TEST KO ! {%d, %d}\n", control.part_1, control.part_2);
+        }
+    }
 
     return 0;
 }
